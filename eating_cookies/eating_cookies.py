0c3dcee1724789613bdb0dd40f85c73c46f28d17
elisamartin <elisamv93@gmail.com>
2019-06-05 16:55:20 +0200

    Eating cookies, with cache

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -7,13 +7,31 @@ import sys
 # recursive solution
 
 
+# def eating_cookies(n, cache=None):
+#     if n == 0:
+#         return 1
+#     elif n < 0:
+#         return 0
+#     else:
+#         return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
+
 def eating_cookies(n, cache=None):
+
     if n == 0:
         return 1
     elif n < 0:
         return 0
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    if cache[n] > 0:
+        return cache[n]
     else:
-        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+        cache[n] = eating_cookies(
+            n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+        return cache[n]
 
 
 if __name__ == "__main__":
