20a8285ea6eb38258ecadbbdd5ce55c83d138671
elisamartin <elisamv93@gmail.com>
2019-06-06 14:44:37 +0200

    Rock Paper Scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,17 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  outcomes = []
+  plays = ['rock', 'paper', 'scissors']
 
+  def find_outcome(rounds_left, result):
+    if rounds_left == 0:
+      return outcomes.append(result)     
+    for play in plays:
+      find_outcome(rounds_left-1, result + [play])
+
+  find_outcome(n, [])
+  return outcomes
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
